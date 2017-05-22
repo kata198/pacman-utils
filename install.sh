50033c50da6dd437b05f6d8a72b325cf27499fb5
Tim Savannah <kata198@gmail.com>
2017-05-22 15:54:27 -0400

    Update install.sh - also set it to install providesDB

--- install.sh
+++ install.sh
@@ -2,6 +2,18 @@
 
 # Installs all the pacman-utils
 
-ALL_FILES="installpackage buildit.sh whatprovides"
+ALL_FILES="installpackage buildpkg.sh whatprovides whatprovides_upstream extractMtree.py"
 
-install -m 755 ${ALL_FILES} "/usr/bin"
+if [ -z "${PREFIX}" ];
+then
+    PREFIX="usr"
+fi
+if [ -z "${DESTDIR}" ];
+then
+    DESTDIR=""
+fi
+
+mkdir -p "${DESTDIR}/${PREFIX}/bin"
+
+install -v -m 755 ${ALL_FILES} "${DESTDIR}/${PREFIX}/bin"
+install -v -m 644 "data/providesDB" "${DESTDIR}/var/lib/pacman/.providesDB"
